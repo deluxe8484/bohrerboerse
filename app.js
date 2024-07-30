556142dcf7b97efa7777e4db6a49f059b1a17258
tinko223306 <valentinspiroski@gmail.com>
2024-07-30 11:10:35 +0200

    Update app.js for equipment
    -V

--- app.js
+++ app.js
@@ -17,7 +17,7 @@ app.set('view engine', 'ejs');
 
 app.use(logger('dev'));
 app.use(express.json());
-app.use(express.urlencoded({ extended: false }));
+app.use(express.urlencoded({ extended: true }));
 app.use(cookieParser());
 app.use(express.static(path.join(__dirname, 'public')));
 
@@ -27,6 +27,7 @@ app.use('/', indexRouter);
 app.use('/users', usersRouter);
 app.use('/tools', equipmentRouter);
 app.use('/borrows', borrowRouter);
+app.use('/equipment', equipmentRouter);
 
 // catch 404 and forward to error handler
 app.use(function(req, res, next) {
